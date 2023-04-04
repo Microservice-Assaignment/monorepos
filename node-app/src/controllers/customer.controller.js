2eb813a8afb21e7da7f1373eebf03b2cae6a62d3
Keanthai <keanthai.thomas@gmail.com>
2023-04-04 12:30:30 +0700

    test

---
+++ node-app/src/controllers/customer.controller.js
@@ -0,0 +1,26 @@
+// //const db = require("../config/db.config")
+
+// // const getList = (req,res)=>{
+// //     db.query("SELECT * FROM customers",(err,res)=>{
+// //        res.json({
+// //         total:4,
+// //         list_customer: rows,
+// //        })
+// //     })
+// // }
+// const create = (req, res) => {
+//   res.send("Controller create customer");
+// };
+// const update = (req, res) => {
+//   res.send("Controller update customer");
+// };
+// const remove = (req, res) => {
+//   res.send("Controller delete customer");
+// };
+
+// module.exports = {
+//   //   getList,
+//   create,
+//   update,
+//   remove,
+// };
---
+++ node-app/src/routes/customer.route.js
@@ -0,0 +1,11 @@
+// const customerController = require("../controllers/customer.controller")
+
+// /// arrow function
+// const customer = (app) =>{
+//     app.get("/api/customer/getlist",customerController.getList)
+//     app.get("/api/customer/create",customerController.create)
+//     app.get("/api/customer/update",customerController.update)
+//     app.get("/api/customer/delete",customerController.remove)
+// }
+
+// module.exports = customer
