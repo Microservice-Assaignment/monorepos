a97d0fd69bdb4e5ffaacbc0f3fcdb602523e038e
Keanthai <keanthai.thomas@gmail.com>
2023-04-26 15:38:25 +0700

    change structure

---
+++ deploy-script.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+ARGS=("$*")
+# workingDir="/microservice/monorepos/microservices"
+workingDir="./microservices"
+docker login -u keanthai -p keanthaihub
+# docker build -t keanthai/haproxy:latest ./haproxy
+
+deploying(){
+    cd "${workingDir}/$1"
+    echo "path --- ${workingDir}/$1"
+    # pwd
+    git pull
+    docker build -t keanthai/$1:latest .
+    docker push keanthai/$1:latest
+    # cd ../..
+}
+echo ${ARGS}
+if [ $ARGS = all ]; 
+then
+    for app in `${workingDir}/`
+    do
+        echo ${app}
+        deploying "${app}"
+    done
+else
+    for ARG in $ARGS
+    do 
+        deploying "${ARG}"
+    done
+fi
+./start.sh
